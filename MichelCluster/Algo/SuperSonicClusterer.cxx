2c0fb656d94672c3591a92aa223822ddc833e8c5
David <devish.c@gmail.com>
2015-08-22 16:35:21 -0500

    adding a clusterer algorithm to find extra hits that belong to the Michel. Idea is there but needs to be implemented -- David Caratelli

---
+++ MichelCluster/Algo/SuperSonicClusterer.cxx
@@ -0,0 +1,53 @@
+#ifndef SUPERSONICCLUSTERER_CXX
+#define SUPERSONICCLUSTERER_CXX
+
+#include "SuperSonicClusterer.h"
+
+namespace michel {
+
+  void SuperSonicClusterer::EventReset()
+  {}
+  
+  void SuperSonicClusterer::Cluster(Michel& michel,
+				    const std::vector<HitPt>& hits){
+    
+
+    
+    //This michel was bogus when it came in, don't cluster further
+    if (michel.size() == 0) return;
+
+    
+    double min_radius = 10; //hardcoded for now
+    double radius = michel._length;
+    
+    //set to min rad
+    if (radius < min_radius) radius = min_radius;
+
+    if(_verbosity <=  msg::kINFO) {
+      std::cout << "\n\n\tRadius clustering"
+		<< "\n\twe use a radius of: " << radius
+		<< "\n\tto cluster the remaining "
+		<< "\n\thits.size() : " << hits.size()
+		<< "\n\tinput\n\n";
+    }
+
+    
+    auto michel_start = michel._start;
+    
+    for (const auto& thishit: hits)
+      if (michel_start.SqDist(thishit) <= radius * radius)
+	michel.push_back(thishit);
+    
+    
+
+    michel._charge = 0;
+    
+    for(const auto& michel_hit : michel)
+      michel._charge += michel_hit._q;
+    
+  }
+
+
+
+}
+#endif
