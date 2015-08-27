8fad925cfc7be271470918ce08bf2f9782ab6d9e
David Kaleko <kaleko@nevis.columbia.edu>
2015-08-27 12:45:04 -0500

    fix to emptymichelID to have it save the associated hits correctly

--- MichelCluster/Algo/EmptyMichelID.cxx
+++ MichelCluster/Algo/EmptyMichelID.cxx
@@ -9,8 +9,14 @@ namespace michel {
 Michel EmptyMichelID::Identify(const MichelCluster& cluster, bool& forward)
 {
 
-  Michel electron;
-  return electron;
+	Michel electron;
+
+	for ( size_t i = 0 ; i < cluster._ordered_pts.size(); ++i) {
+		electron.push_back(cluster._hits[cluster._ordered_pts[i]]);
+	}
+
+
+	return electron;
 }
 
 }
---
+++ MichelCluster/mac/save_michel_clusters_mergedebug.py
@@ -0,0 +1,93 @@
+import sys
+
+if len(sys.argv) < 2:
+    msg  = '\n'
+    msg += "Usage 1: %s $INPUT_ROOT_FILE(s)\n" % sys.argv[0]
+    msg += '\n'
+    sys.stderr.write(msg)
+    sys.exit(1)
+
+import ROOT
+ROOT.gSystem.Load("libMichelReco_MichelClusterFmwk")
+from larlite import larlite as fmwk
+from ROOT import michel
+
+# Create ana_processor instance
+my_proc = fmwk.ana_processor()
+
+# Set input root file
+for x in xrange(len(sys.argv)-1):
+    my_proc.add_input_file(sys.argv[x+1])
+
+my_proc.set_io_mode(fmwk.storage_manager.kBOTH)
+#my_proc.set_io_mode(fmwk.storage_manager.kREAD)
+
+#my_proc.set_ana_output_file("michel_tree.root")
+
+my_proc.set_output_file("michel_clusters.root")
+
+#Cheat and send in signal
+my_proc.enable_filter(True)
+the_filter = fmwk.MichelFilter()
+#the_filter = fmwk.RemoveMichel()
+
+# Michel reco driver code
+my_unit = fmwk.MichelRecoDriver()
+my_unit.SetClusterProducer("fuzzycluster")
+#my_unit.SetClusterProducer("rawcluster")
+my_unit.SetEField(0.5)
+
+# set here if you want to save michels as an output cluster
+my_unit.saveMichelClusters(True)
+
+# Get manager for michel reco
+mgr = my_unit.Algo()
+
+#mgr.SetVerbosity(michel.msg.kDEBUG)
+
+# Attach algorithm for merging
+mgr.SetAlgo(michel.kClusterMerger, 
+            michel.EdgeMerger())
+
+mgr.SetAlgo(michel.kBoundaryFinder,
+            michel.EmptyBoundary())
+
+mgr.SetAlgo(michel.kMIDFilter,
+            michel.EmptyMIDFilter())
+
+mgr.SetAlgo(michel.kMichelID, 
+            michel.EmptyMichelID())
+
+mgr.SetAlgo(michel.kMichelCluster, 
+            michel.EmptyMichelCluster())
+
+
+# # Attach ana unit
+
+# mgr.AddAna(michel.CosmicAna())
+
+# add process to get moving
+#my_proc.add_process(the_filter)
+my_proc.add_process(my_unit)
+
+my_proc.set_data_to_write(fmwk.data.kHit,'cchit')
+my_proc.set_data_to_write(fmwk.data.kCluster,'michel')
+#my_proc.set_data_to_write(fmwk.data.kCluster,'rawclusters')
+my_proc.set_data_to_write(fmwk.data.kAssociation,'michel')
+#my_proc.set_data_to_write(fmwk.data.kAssociation,'rawclusters')
+#Write aho unit to get out MC vars whatever
+
+print
+print  "Finished configuring ana_processor. Start event loop!"
+print
+
+# Let's run it.
+#my_proc.run(145,5);
+my_proc.run()
+
+# done!
+print
+print "Finished running ana_processor event loop!"
+print
+
+sys.exit(0)
