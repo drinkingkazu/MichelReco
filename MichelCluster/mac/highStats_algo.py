81f371427ee1c2af17539ba40d6c64175e4c9031
David Caratelli <davidc1@uboonegpvm06.fnal.gov>
2015-09-24 12:06:34 -0500

    updating high stats algo -- David Caratelli

---
+++ MichelCluster/mac/highStats_algo.py
@@ -0,0 +1,119 @@
+import ROOT
+from ROOT import michel
+import parse_fiducial_volume_definitions as fidparser
+
+def PrepareMichelAlgo():
+
+    algoList = []
+    #########################################
+    # calculate various cluster parameters...
+    ctrunk = michel.CalcTruncated()
+    #ctrunk.SetCovarianceWindowSize(int s)      
+    #ctrunk.SetTruncatedQWindowSize(int s)      
+    #ctrunk.SetPAbove(double p)                 
+    #ctrunk.SetMinWindowSize(int w)             
+    #ctrunk.SetEdgeEffectFix(int e)             
+    algoList.append(ctrunk)
+
+    ########################################
+    # Attach algorithm for boundary finding
+    boundaryalgo = michel.BoundaryFromTQMaxQ()
+    boundaryalgo.SetMaxDistancesTruncatedQMaxQ(15)
+    algoList.append(boundaryalgo)
+
+    ##############################################
+    # Attach algo for charge spectrum requirements
+    closepeaks = michel.RequireCloseTruncatedPeaks()
+    closepeaks.SetMaxDistanceTruncatedPeaks(5)
+    #algoList.append(closepeaks)
+
+    covdip = michel.RequireCovarianceDip()
+    covdip.SetCovarianceDipCutoff(0.9)
+    #algoList.append(covdip)
+
+    slopeflip = michel.RequireSlopeSignFlip()
+    #algoList.append(slopeflip)
+
+    lowcovbound = michel.RequireBoundaryInLowCov()
+    lowcovbound.SetMaxCovarianceAtStart(0.8)
+    algoList.append(lowcovbound)
+
+    #############################################
+    # Attach algorithm for finding michel cluster
+    findMichel = michel.ForwardMichelID()
+    findMichel.SetMaxMichelHits(0)
+    algoList.append(findMichel)
+
+    #########################
+    # MID finding algorithms
+    midalgo = michel.DecideIfStoppingMuon()
+    midalgo.SetChiMin     ( 0.9 )
+    midalgo.SetFracMinHits( 0.7 )
+    midalgo.SetHitRadius  ( 30  )
+    midalgo.SetMaxDist    ( 3.0 )
+    midalgo.SetMinBadHits ( 10  )
+    algoList.append(midalgo)
+
+    ############################
+    # MINIMUM LENGTH REQUIREMENT
+    minlength = michel.CutOnMuonLength()
+    minlength.SetMinMuonLength(10)
+    algoList.append(minlength)
+
+    ############################
+    # MUON LINEARITY CUT
+    minlinearity = michel.CutOnMuonLinearity()
+    minlinearity.SetChiMin     ( 0.8 )
+    minlinearity.SetFracMinHits( 0.5 )
+    algoList.append(minlinearity)
+
+    #########################################################
+    # MID filter that removes michels close to wire gaps/edges
+    fidvolfilter = michel.CutOnFiducialVolume()
+
+    wires_to_exclude_min, \
+        wires_to_exclude_max, \
+        times_to_exclude_min, \
+        times_to_exclude_max = fidparser.list_wires_times_to_exclude()
+    
+    fidvolfilter.SetExcludedWireRanges(wires_to_exclude_min,wires_to_exclude_max)
+    fidvolfilter.SetExcludedTimeRanges(times_to_exclude_min,times_to_exclude_max)
+    algoList.append(fidvolfilter)
+
+    ######################################
+    # Attach algorithm to recluster michel
+    supersonic = michel.SuperSonicClusterer()
+    supersonic.SetMergeTillConverge(True)
+    supersonic.SetUseHitRadius     (True)
+    supersonic.SetMaxRadius( 15 )
+    supersonic.SetHitRadius(  3 )
+    algoList.append(supersonic)
+
+    #########################
+    # cone-finding algorithm
+    conefinder = michel.ConeHitFinder()
+    conefinder.SetMaxRadius               ( 20 )
+    conefinder.SetMaxPerpendicularDistance(  3 )
+    algoList.append(conefinder)
+
+    ##############################################
+    # final mid algo cutting on num of michel hits
+    michelhits = michel.CutOnMichelNumHits()
+    michelhits.SetMinMichelHits (  5 )
+    michelhits.SetMaxMichelHits ( 35 )
+    algoList.append(michelhits)
+
+    #########################################
+    # remove weird horizontal tracks from PMT
+    pmtremoved = michel.RemoveFakePMTSignals()
+    pmtremoved.SetMaxErrorTime(0.1)
+    algoList.append(pmtremoved)
+
+    #############################################
+    # require large angle between michel and muon
+    largeangle = michel.RequireLargeAngle()
+    largeangle.SetMinAngle(30.*3.14/180.)
+    largeangle.SetMinStraightMichelHits(5)
+    algoList.append(largeangle)
+
+    return algoList
--- MichelCluster/mac/test_michel.py
+++ MichelCluster/mac/test_michel.py
@@ -11,6 +11,7 @@ import ROOT
 ROOT.gSystem.Load("libMichelReco_MichelClusterFmwk")
 from larlite import larlite as fmwk
 from ROOT import michel
+from highStats_algo import PrepareMichelAlgo
 
 # Create ana_processor instance
 my_proc = fmwk.ana_processor()
@@ -29,13 +30,14 @@ my_proc.set_output_file    ("michel_clusters.root")
 my_unit = fmwk.MichelRecoDriver()
 #my_unit.SetClusterProducer("fuzzycluster")
 my_unit.SetClusterProducer("rawcluster")
+#my_unit.SetClusterProducer("linecluster")
 my_unit.SetUseMC(False)
 my_unit.SetEField(0.27)
 my_unit.SetMinClusSize(15)
 
 ###########################################################
 # set here if you want to save michels as an output cluster
-# my_unit.saveMichelClusters(True)
+#my_unit.saveMichelClusters(True)
 
 #############################
 # Get manager for michel reco
@@ -48,115 +50,13 @@ mgr = my_unit.GetManager()
 
 ##############################
 # Attach algorithm for merging
-#mgr.AddMergingAlgo(michel.EdgeMerger())
+mgr.AddMergingAlgo(michel.EdgeMerger())
 
-#########################################
-# calculate various cluster parameters...
-ctrunk = michel.CalcTruncated()
-#ctrunk.SetCovarianceWindowSize(int s)      
-#ctrunk.SetTruncatedQWindowSize(int s)      
-#ctrunk.SetPAbove(double p)                 
-#ctrunk.SetMinWindowSize(int w)             
-#ctrunk.SetEdgeEffectFix(int e)             
-mgr.AddAlgo(ctrunk)
 
-########################################
-# Attach algorithm for boundary finding
-boundaryalgo = michel.BoundaryFromTQMaxQ()
-boundaryalgo.SetMaxDistancesTruncatedQMaxQ(15)
-mgr.AddAlgo(boundaryalgo)
+algoList = PrepareMichelAlgo()
 
-##############################################
-# Attach algo for charge spectrum requirements
-closepeaks = michel.RequireCloseTruncatedPeaks()
-closepeaks.SetMaxDistanceTruncatedPeaks(5)
-#mgr.AddAlgo(closepeaks)
-
-covdip = michel.RequireCovarianceDip()
-covdip.SetCovarianceDipCutoff(0.9)
-#mgr.AddAlgo(covdip)
-
-slopeflip = michel.RequireSlopeSignFlip()
-#mgr.AddAlgo(slopeflip)
-
-lowcovbound = michel.RequireBoundaryInLowCov()
-lowcovbound.SetMaxCovarianceAtStart(0.8)
-mgr.AddAlgo(lowcovbound)
-
-#############################################
-# Attach algorithm for finding michel cluster
-findMichel = michel.ForwardMichelID()
-findMichel.SetMaxMichelHits(0)
-mgr.AddAlgo(findMichel)
-
-#########################
-# MID finding algorithms
-midalgo = michel.DecideIfStoppingMuon()
-midalgo.SetChiMin     ( 0.9 )
-midalgo.SetFracMinHits( 0.7 )
-midalgo.SetHitRadius  ( 30  )
-midalgo.SetMaxDist    ( 3.0 )
-midalgo.SetMinBadHits ( 10  )
-mgr.AddAlgo(midalgo)
-
-minlength = michel.CutOnMuonLength()
-minlength.SetMinMuonLength(10)
-mgr.AddAlgo(minlength)
-
-minlinearity = michel.CutOnMuonLinearity()
-minlinearity.SetChiMin     ( 0.8 )
-minlinearity.SetFracMinHits( 0.5 )
-mgr.AddAlgo(minlinearity)
-
-#########################################################
-# MID filter that removes michels close to wire gaps/edges
-fidvolfilter = michel.CutOnFiducialVolume()
-import parse_fiducial_volume_definitions as fidparser
-
-wires_to_exclude_min, \
-    wires_to_exclude_max, \
-    times_to_exclude_min, \
-    times_to_exclude_max = fidparser.list_wires_times_to_exclude()
-
-fidvolfilter.SetExcludedWireRanges(wires_to_exclude_min,wires_to_exclude_max)
-fidvolfilter.SetExcludedTimeRanges(times_to_exclude_min,times_to_exclude_max)
-mgr.AddAlgo(fidvolfilter)
-
-######################################
-# Attach algorithm to recluster michel
-supersonic = michel.SuperSonicClusterer()
-supersonic.SetMergeTillConverge(True)
-supersonic.SetUseHitRadius     (True)
-supersonic.SetMaxRadius( 15 )
-supersonic.SetHitRadius(  3 )
-mgr.AddAlgo(supersonic)
-
-#########################
-# cone-finding algorithm
-conefinder = michel.ConeHitFinder()
-conefinder.SetMaxRadius               ( 20 )
-conefinder.SetMaxPerpendicularDistance(  3 )
-mgr.AddAlgo(conefinder)
-
-##############################################
-# final mid algo cutting on num of michel hits
-michelhits = michel.CutOnMichelNumHits()
-michelhits.SetMinMichelHits (  5 )
-michelhits.SetMaxMichelHits ( 35 )
-mgr.AddAlgo(michelhits)
-
-#########################################
-# remove weird horizontal tracks from PMT
-pmtremoved = michel.RemoveFakePMTSignals()
-pmtremoved.SetMaxErrorTime(0.1)
-mgr.AddAlgo(pmtremoved)
-
-#############################################
-# require large angle between michel and muon
-largeangle = michel.RequireLargeAngle()
-largeangle.SetMinAngle(30.*3.14/180.)
-largeangle.SetMinStraightMichelHits(5)
-mgr.AddAlgo(largeangle)
+for algo in algoList:
+    mgr.AddAlgo(algo)
 
 # Attach ana unit
 mgr.AddAna(michel.CosmicAna())
@@ -164,7 +64,8 @@ mgr.AddAna(michel.CosmicAna())
 # add process
 my_proc.add_process(my_unit)
 
-my_proc.set_data_to_write(fmwk.data.kHit,'cchit')
+#my_proc.set_data_to_write(fmwk.data.kHit,'cchit')
+my_proc.set_data_to_write(fmwk.data.kHit,'linecluster')
 #my_proc.set_data_to_write(fmwk.data.kHit,'gaushit')
 my_proc.set_data_to_write(fmwk.data.kCluster,'michel')
 #my_proc.set_data_to_write(fmwk.data.kCluster,'rawclusters')
